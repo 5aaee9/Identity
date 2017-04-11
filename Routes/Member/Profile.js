07f1d1b3bf59b4daae99d05ef12990c8ba13aa7c
Indexyz <jiduye@gmail.com>
2017-04-11 19:11:35 +0800

    Reload Progress Finished

--- Routes/Member/Profile.js
+++ Routes/Member/Profile.js
@@ -15,3 +15,37 @@ module.exports.get = (req, res, next) => {
         })
     });
 };
+
+module.exports.post = (req, res, next) => {
+    let type = req.body.type,
+        userModel = db.model("users", userSchema);
+
+    userModel.findOne({
+        _id: req.session.user._id
+    }, (err, doc) => {
+        let funcs = {
+            "resetUserKey": (func) => { doc.refresh(); func(undefined, "保存成功") },
+            "changeUserName": (func) => {
+                let username = req.body.username;
+                if (username.length < 3){ func("用户名长度太小");return }
+                doc.username = username;
+                func(undefined, "修改成功")
+            }
+        };
+        funcs[type]((err, info) => {
+            let ret = (err, info) => {
+                res.render("member/profile", {
+                    user: doc,
+                    e: err,
+                    info: info
+                })
+            };
+            if (err) { ret(err, null) }
+            doc.save(err => {
+                if (err) { ret(err.message); return }
+                ret(undefined, info)
+            })
+        })
+
+    });
+};
--- Routes/Member/Urls.js
+++ Routes/Member/Urls.js
@@ -7,5 +7,6 @@ let router = express.Router();
 
 router.get("/", require("./Index").get);
 router.get("/profile", require("./Profile").get);
+router.post("/profile", require("./Profile").post);
 
 module.exports = router;
