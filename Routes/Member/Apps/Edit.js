12e1f5f7b214a1baa459d3d6629f0102be94cea9
Indexyz <jiduye@gmail.com>
2017-05-21 12:54:19 +0800

    Finish Appliction Edit

--- Routes/Member/Apps/Edit.js
+++ Routes/Member/Apps/Edit.js
@@ -5,9 +5,14 @@ const db = require('mongoose');
 const apps = require('../../../Db/Schema/Application');
 const dbEnum = require('../../../Define/Db');
 const userAuth = require('../../../Db/Schema/UserAuth');
+const grid = require('gridfs-stream');
+const fs = require("fs");
 
 let appModel = db.model(dbEnum.Db.APPS_DB, apps),
     userAuthModel = db.model(dbEnum.Db.APP_USER_DB, userAuth);
+
+grid.mongo = db.mongo;
+let gfs = grid(db.connection.db);
 
 module.exports.get = (req, res, next) => {
     let appId = req.params["appId"];
@@ -26,7 +31,49 @@ module.exports.get = (req, res, next) => {
     })
 };
 
+
+let uploadFile = (file, func) => {
+    let writeStream = gfs.createWriteStream({
+        filename: file.filename + ".png",
+        mode: 'w',
+        content_type: file.mimetype
+    });
+    writeStream.on('close', filen => {
+        fs.unlink(file.path, err => {
+            func(err, filen)
+        })
+    });
+    fs.createReadStream(file.path).pipe(writeStream);
+};
+
 module.exports.post = (req, res, next) => {
-    console.log(req.file)
-    res.send("1")
+    let appName = req.body["appname"],
+        homepage = req.body["homepage"],
+        callback = req.body["callback"],
+        appId = req.params["appId"];
+
+    let _event = fileId => {
+        appModel.findOne({
+            _id: appId,
+            owner: req.session.user._id
+        }, (err, doc) => {
+            if (err || !doc) { return res.redirect("/member/apps") }
+            doc.image = fileId;
+            doc.name = appName;
+            doc.homePage = homepage;
+            doc.redirectUri = callback;
+            doc.save(err => {
+                if (err) { return next(err) }
+                res.redirect("/member/apps?success=" + encodeURIComponent("修改成功: " + doc.name))
+            });
+        })
+    };
+
+    if (req.file) {
+        uploadFile(req.file, (err, file) => {
+            _event(file._id)
+        })
+    } else {
+        _event()
+    }
 };
