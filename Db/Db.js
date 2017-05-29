148400345a5c22efadae79fdf9651eff4f45ee63
Indexyz <jiduye@gmail.com>
2017-05-30 03:22:43 +0800

    Simple mongo db config

--- Db/Db.js
+++ Db/Db.js
@@ -5,23 +5,10 @@ const mongoose = require('mongoose');
 
 function getUrl() {
     let mongodbUri = 'mongodb://';
-    if (!process.env.MONGODB_PORT_27017_TCP_ADDR) {
-        if (config.db_auth){
-            mongodbUri += `${config.db_username}:${config.db_password}@${config.db_host}:${config.db_port}/${config.db_name}`
-        } else {
-            mongodbUri += `${config.db_host}:${config.db_port}/${config.db_name}`
-        }
+    if (config.db_auth){
+        mongodbUri += `${config.db_username}:${config.db_password}@${config.db_host}:${config.db_port}/${config.db_name}`
     } else {
-        if (process.env.MONGODB_USERNAME) {
-            mongodbUri += process.env.MONGODB_USERNAME;
-            if (process.env.MONGODB_PASSWORD) {
-                mongodbUri += ":" + process.env.MONGODB_PASSWORD
-            }
-            mongodbUri += "@";
-        }
-        mongodbUri += (process.env.MONGODB_PORT_27017_TCP_ADDR || '1.2.3.4')
-            + ":" + (process.env.MONGODB_PORT_27017_TCP_PORT || 27017)
-            + '/' + (process.env.MONGODB_INSTANCE_NAME || 'test');
+        mongodbUri += `${config.db_host}:${config.db_port}/${config.db_name}`
     }
     return mongodbUri
 }
