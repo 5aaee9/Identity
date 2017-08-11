9a022d3dc995b9c97713b9dec6278064e0319cf8
Indexyz <jiduye@gmail.com>
2017-08-12 03:16:43 +0800

    [Style] add raven to track exception in production

--- App.js
+++ App.js
@@ -6,15 +6,31 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const git = require('git-rev');
 const cookieSession = require('cookie-session');
 const config = require('./Config');
 const db = require('mongoose');
+const Raven = require('raven');
 const app = express();
 const moment = require('./Libs/moment.min');
 const link = require("./Db/Redis").link;
 const dbDefine = require("./Define/Db").Db;
 const i18n = require("./i18n");
 const userService = require("./Db/Service/userService");
+
+if (process.env["RAVEN_TOKEN"]) {
+    git.long(function (str) {
+        Raven.config(
+            process.env["RAVEN_TOKEN"], {
+                tags: {
+                    git_commit: str ? str : undefined
+                },
+                release: str ? str : undefined
+            }
+        ).install();
+    })
+}
+
 
 app.use(logger('[:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
 
@@ -68,6 +84,13 @@ require("./Db/Db")(() => {
                     user.profile = doc;
                     user.username = doc.UserName;
                     res.locals.user = user;
+                    Raven.setContext({
+                        user: {
+                            id: user._id,
+                            username: doc.UserName,
+                            email: user.email
+                        }
+                    })
                     next()
                 }, err => {
                     next(err)
@@ -86,6 +109,10 @@ require("./Db/Db")(() => {
         next()
     });
 
+    if (process.env["RAVEN_TOKEN"]) {
+        app.use(Raven.requestHandler());
+    }
+
     // router
     app.use(require('./Routes/Urls'));
 
@@ -93,12 +120,19 @@ require("./Db/Db")(() => {
     app.use(function (req, res, next) {
         let err = new Error('Not Found');
         err.status = 404;
-        next(err);
+        throw err;
     });
+
+    if (process.env["RAVEN_TOKEN"]) {
+        app.use(Raven.errorHandler());
+    }
 
     // error handle
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
+        if (process.env["RAVEN_TOKEN"] && res.sentry) {
+            res.setHeader("X-Error-Id", res.sentry);
+        }
         res.render('error', {
             message: err.message,
             error  : err
