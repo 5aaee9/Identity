6fa04e9b8c50efc1d24f3b0188daa988d475cbc5
Indexyz <jiduye@gmail.com>
2017-08-12 03:40:41 +0800

    [Fix] Fix Admin Test

--- Tests/Admin.js
+++ Tests/Admin.js
@@ -4,13 +4,18 @@ const request = require("supertest");
 const stringLib = require('../Utils/String');
 const dbDefine = require("../Define/Db");
 const userS = require("../Db/Schema/User");
+const Tsession = require('supertest-session');
 
 let application = require("../App"),
     userService = require("../Db/Service/userService"),
     userModel = db.model(dbDefine.Db.USER_DB, userS);
 
 describe("Admin", function() {
-    var userName, email, password, user, cookies;
+    var userName, email, password, user, cookies, session;
+    before(function(done) {
+        session = Tsession(application);
+        done()
+    })
     beforeEach(function(done) {
         userName = stringLib.randomString(12)
         password = stringLib.randomString(12)
@@ -25,7 +30,16 @@ describe("Admin", function() {
                 u.save(err => {
                     if (err) return done(err);
                     user = u
-                    done()
+                    session.post('/auth/login')
+                        .send({
+                            email: user.email,
+                            password: password
+                        })
+                        .expect(302)
+                        .end(function(err, res){
+                            if(err) return done(err);
+                            done();
+                        });
                 })
             }
         )
@@ -36,10 +50,21 @@ describe("Admin", function() {
             request(application).get("/admin").expect(302).end(done)
         })
         it("Test not admin", function(done){
-
+            userModel.findById(user._id, (err, res) => {
+                if (err) return done(err);
+                res.isAdmin = false;
+                res.save(() => {
+                    session.get("/admin")
+                        .expect(200)
+                        .end(done)
+                })
+            })
         })
         it("Test admin", function(done){
 
+            session.get("/admin")
+                .expect(200)
+                .end(done)
         })
     })
 
